21c57c747876de8752031b5a7d24c284f4312f8a
geol2 <big9401@gmail.com>
2023-06-20 23:55:18 +0900

    크롤링 예제 추가

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # crawling-meme
 use python
+
+```shell
+pip install selenium
+pip install --upgrade pip
+```
+
+## 크롬 드라이버 설치
+
+- 사용한 버전
+
+```text
+ChromeDriver 114.0.5735.90
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import math
+
+driver = webdriver.Chrome()
+driver.get("https://m.place.naver.com/place/1342356243/review/ugc?entry=pll&zoomLevel=12.000&type=photoView")
+
+driver.implicitly_wait(0.5)
+
+findBlogReviewString = "블로그리뷰"
+findSeparator = " "
+
+
+def findTotal():
+    totalCount = 0
+    blueLink = driver.find_elements(By.CLASS_NAME, "place_bluelink")
+    for e in blueLink:
+        blogReviewIndex = e.text.find(findBlogReviewString)
+        if blogReviewIndex == 0:
+            findTotalCount = e.text.split(" ")
+            totalCount = findTotalCount[1]
+
+    return totalCount
+
+
+def allDisplay(clickNumber):
+    # 더보기 없어질 때 까지 계속하기
+    for _ in range(clickNumber):
+        driver.implicitly_wait(1)
+        aTag = driver.find_element(By.CLASS_NAME, "fvwqf")
+        aTag.click()
+
+def getClickNumber(totalCount):
+    return math.ceil(int(totalCount) / 10)
+
+
+totalCount = findTotal()
+clickNumber = getClickNumber(totalCount)
+allDisplay(clickNumber)
+
+driver.quit()
