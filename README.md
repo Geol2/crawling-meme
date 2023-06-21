dbe5e43877e4174febc67cc066b4ee135e7485e8
Geol2 <big9401@gmail.com>
2023-06-21 11:10:10 +0900

    MOD : sleep 및 클릭 카운트 개수 수정

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # crawling-meme
 use python
 
+1. python 3.8 & 3.9
+2. pip ([get-pip.py](https://bootstrap.pypa.io/get-pip.py))
+
 ```shell
+# mac, linux
 pip install selenium
 pip install --upgrade pip
+
+# windows pycharm
+# pycharm 내에서 별도 selenium 패키지 설치
 ```
 
 ## 크롬 드라이버 설치
--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import math
-
-driver = webdriver.Chrome()
-driver.get("https://m.place.naver.com/place/1342356243/review/ugc?entry=pll&zoomLevel=12.000&type=photoView")
-
-driver.implicitly_wait(1)
+import time
 
 findBlogReviewString = "블로그리뷰"
 findSeparator = " "
+waitTime = 10
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(waitTime)
+driver.get("https://m.place.naver.com/place/1342356243/review/ugc?entry=pll&zoomLevel=12.000&type=photoView")
+driver.implicitly_wait(waitTime)
 
 
 def findTotal():
+    # 블로그 수 전체를 찾는 함수
     totalCount = 0
     blueLink = driver.find_elements(By.CLASS_NAME, "place_bluelink")
     for e in blueLink:
@@ -25,19 +28,24 @@ def findTotal():
 
 
 def allShowDisplay(clickNumber):
+    i = 0;
     # 더보기 없어질 때 까지 계속하기
     for _ in range(clickNumber):
-        driver.implicitly_wait(1)
         aTag = driver.find_element(By.CLASS_NAME, "fvwqf")
         aTag.click()
+        time.sleep(1)
+        i += 1
 
+    return i
 
 def getClickNumber(totalCount):
-    return math.ceil(int(totalCount) / 10)
+    # 더보기 클릭을 몇 번 해야하는지 찾는 함수
+    # 첫 실행 시, 최대 10개는 노출해주므로 전체에서 1번은 클릭을 해주지 않아도 된다
+    return math.ceil(int(totalCount) / 10) - 1
 
 
 totalCount = findTotal()
 clickNumber = getClickNumber(totalCount)
-allShowDisplay(clickNumber)
+showCount = allShowDisplay(clickNumber)
 
 driver.quit()
