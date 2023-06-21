246ed2be68a2ed12be248960b64aceddb2e83858
Geol2 <big9401@gmail.com>
2023-06-21 11:25:16 +0900

    MOD : _ 이름 변경

--- main.py
+++ main.py
@@ -4,48 +4,48 @@ from selenium.webdriver.common.by import By
 import math
 import time
 
-findBlogReviewString = "블로그리뷰"
-findSeparator = " "
-waitTime = 10
+blog_review_string = "블로그리뷰"
+separator = " "
+wait_time = 1
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(waitTime)
+driver.implicitly_wait(wait_time)
 driver.get("https://m.place.naver.com/place/1342356243/review/ugc?entry=pll&zoomLevel=12.000&type=photoView")
-driver.implicitly_wait(waitTime)
 
 
-def findTotal():
+def find_total():
     # 블로그 수 전체를 찾는 함수
-    totalCount = 0
-    blueLink = driver.find_elements(By.CLASS_NAME, "place_bluelink")
-    for e in blueLink:
-        blogReviewIndex = e.text.find(findBlogReviewString)
-        if blogReviewIndex == 0:
-            findTotalCount = e.text.split(" ")
-            totalCount = findTotalCount[1]
+    total_count = 0
+    blue_link = driver.find_elements(By.CLASS_NAME, "place_bluelink")
+    for e in blue_link:
+        blog_review_index = e.text.find(blog_review_string)
+        if blog_review_index == 0:
+            find_total_count = e.text.split(separator)
+            total_count = find_total_count[1]
 
-    return totalCount
+    return total_count
 
 
-def allShowDisplay(clickNumber):
+def show_blog_list(number):
     i = 0;
     # 더보기 없어질 때 까지 계속하기
-    for _ in range(clickNumber):
-        aTag = driver.find_element(By.CLASS_NAME, "fvwqf")
-        aTag.click()
+    for _ in range(number):
+        a_tag = driver.find_element(By.CLASS_NAME, "fvwqf")
+        a_tag.click()
         time.sleep(1)
         i += 1
 
     return i
 
-def getClickNumber(totalCount):
+
+def get_click_number(total_count):
     # 더보기 클릭을 몇 번 해야하는지 찾는 함수
     # 첫 실행 시, 최대 10개는 노출해주므로 전체에서 1번은 클릭을 해주지 않아도 된다
-    return math.ceil(int(totalCount) / 10) - 1
+    return math.ceil(int(total_count) / 10) - 1
 
 
-totalCount = findTotal()
-clickNumber = getClickNumber(totalCount)
-showCount = allShowDisplay(clickNumber)
+totalCount = find_total()
+clickNumber = get_click_number(totalCount)
+showCount = show_blog_list(clickNumber)
 
 driver.quit()
