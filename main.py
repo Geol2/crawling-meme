8fcd158eff3964f0cb82cf11d17d576584805dd1
Geol2 <big9401@gmail.com>
2023-06-26 10:38:39 +0900

    main 함수 구현

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import getopt
+import sys
+
 from libs import common
 from libs import crawling
 
-crawl = crawling.NaverCrawling()
-crawling.NaverCrawling().tennis_blog_service()
 
-common.logger.info("END CRAWLING")
-crawl.driver.quit()
+def main(argv):
+    opts = getopt.getopt(argv[:1])
+    crawl = ''
+
+    for opt, arg in opts:
+        if opt in ("-blog"):
+            crawl = crawling.NaverCrawling()
+            crawling.NaverCrawling().tennis_blog_service()
+        elif opt in ("-url"):
+            # 특정 url을 가져와서 크롤링하기
+            crawl = crawling.NaverCrawling()
+            return 0
+
+    common.logger.info("END CRAWLING")
+    crawl.driver.quit()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
