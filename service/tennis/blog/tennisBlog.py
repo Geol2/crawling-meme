0c7a305e459df5535e05418cb59452f81822a747
Geol2 <big9401@gmail.com>
2023-07-06 18:21:12 +0900

    MOD : 레슨 테니스장 수정작업

--- service/tennis/blog/tennisBlog.py
+++ service/tennis/blog/tennisBlog.py
@@ -10,7 +10,7 @@ class TennisBlog(tennisFactory.Tennis):
             is_exist_blog = db.mysql.is_exist_blog(db.cursor, url)
             if is_exist_blog is True:
                 # 존재하고 있으면 1로 업데이트
-                db.mysql.update_checkout_blog(url)
+                db.mysql.set_blog_info(url)
             else:
                 # 존재하지 않으면 insert
                 self.insert_and_increase_tennis_blog(seq)
