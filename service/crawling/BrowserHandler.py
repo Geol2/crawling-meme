477717b924e1d235508bb77c176d15dc8c3ce0c6
Geol2 <big9401@gmail.com>
2023-09-01 12:22:43 +0900

    * 크롬 헤드리스 수정 chrome headless=new

--- service/crawling/BrowserHandler.py
+++ service/crawling/BrowserHandler.py
@@ -15,7 +15,7 @@ class BrowserHandler:
 
     def chrome(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--blink-settings=imagesEnabled=false')
